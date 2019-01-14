dc5118b71e2b2e21630ef0764c5cd64a4e307b04
Anders Bjerner <abjerner@skybrud.dk>
2019-01-14 23:13:02 +0100

    Cleanup

---
+++ src/Skybrud.Umbraco.Analytics/App_Plugins/Skybrud.Analytics/Scripts/Controllers/Overlays/OAuth.js
@@ -0,0 +1,29 @@
+﻿angular.module("umbraco").controller("Skybrud.Analytics.OAuthOverlay", function ($scope, editorState, userService, $http, analyticsService) {
+
+    // Define an alias for the editor (eg. used for callbacks)
+    var alias = $scope.alias = ("skybrudanalytics_" + Math.random()).replace(".", "");
+
+    $scope.clients = [];
+    $scope.mode = "select";
+
+    analyticsService.getClients().then(function (res) {
+        $scope.clients = res.data;
+    });
+
+    $scope.selectClient = function (client) {
+
+        //$scope.mode = "authenticate";
+        $scope.model.client = client;
+
+        var url = "/App_Plugins/Skybrud.Analytics/Dialogs/GoogleOAuth.aspx?callback=" + alias + "&id=" + client.id;
+        window.open(url, "Google OAuth", "scrollbars=no,resizable=yes,menubar=no,width=800,height=700");
+
+    };
+
+    window[alias] = function (user) {
+        $scope.model.user = user;
+        $scope.mode = "authenticated";
+        $scope.model.disableSubmitButton = user === null;
+    }
+
+});
