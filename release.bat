68ed3d008c249fe00366b9b29d842f5c731ce292
Anders Bjerner <abjerner@skybrud.dk>
2020-01-30 23:08:08 +0100

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,2 @@
 @echo off
-cd src/Skybrud.Umbraco.Analytics
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
-REM cd ../../
-REM grunt
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.Umbraco.Analytics /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
